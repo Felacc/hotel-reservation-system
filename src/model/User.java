2bd647345d4e57cd5a3f26e73c6938ef93f0659d
Caleb Irvine <irvinecaleb1@gmail.com>
2025-03-02 23:14:49 +0000

    Update 4 files
    - /src/abstractuser/Guest.java
    - /src/abstractuser/User.java
    - /src/model/Guest.java
    - /src/model/User.java

--- src/model/User.java
+++ src/model/User.java
@@ -1,13 +1,68 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package model;
+package abstractuser;
 
 /**
  *
- * @author kale
+ * @author Edwynne Kalugdan
  */
-public class User {
-    
+public abstract class User {
+    private String email;
+    private String phoneNumber;
+    private String address;
+    private String password;
+    private int userID;
+
+    // Constructor
+    public User(String email, String phoneNumber, String address, String password, int userID) {
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+        this.address = address;
+        this.password = password;
+        this.userID = userID;
+    }
+
+    // Abstract methods to be implemented by subclasses
+    public abstract void login();
+    public abstract void updateInfo(String newEmail, String newPhoneNumber, String newAddress);
+
+    // Getters and setters
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public int getUserID() {
+        return userID;
+    }
+
+    public void setUserID(int userID) {
+        this.userID = userID;
+    }
 }
+
