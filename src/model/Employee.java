c1d6cf43db35ec2f76ca2cd3384dbb95b03df2de
Felacc <98856469+Felacc@users.noreply.github.com>
2025-03-05 10:28:54 -0800

    Refactor Admin from interface to class
    Changed the structure of the model package a bit regarding admins and employees.
    Made Admin a class that extends employee and not an interface.
    Employee class now has protected setters so that admins can easily set their SIN
    and citizenship status. Employees can also adjust their SIN in theory, but if we
    are careful of how we implement the functionality in the GUI, then only admins will
    be able to change employee information.

--- src/model/Employee.java
+++ src/model/Employee.java
@@ -10,14 +10,44 @@ package model;
  */
 public class Employee extends User implements Staff {
     private int SIN;
-    private String hireDate; // will possibly want to refactor to use date type and instatiate with the current date
+    private String citizenshipStatus;
+    final private String hireDate; // will possibly want to refactor to use date type and instantiate objects with the current date
 
-    public Employee(String firstName, String lastName, String email, String phoneNumber, String address, String password, int SIN, String hireDate) {
+    public Employee(String firstName, String lastName, String email, String phoneNumber, String address, String password, int SIN, String citizenshipStatus, String hireDate) {
         super(firstName, lastName, email, phoneNumber, address, password);
         this.SIN = SIN;
+        this.citizenshipStatus = citizenshipStatus;
         this.hireDate = hireDate;
     }
 
+    
+    // Getters
+
+    public int getSIN() {
+        return SIN;
+    }
+
+    public String getCitizenshipStatus() {
+        return citizenshipStatus;
+    }
+
+    public String getHireDate() {
+        return hireDate;
+    }
+    
+    
+    // Protected setters
+    // Only employees and admins can set employee specific attributes
+    // Hence the protected visibility modifier
+    // there isn't one for hire date because I am going to refactor hire date to set itself to the current date at time of object instantiation
+    protected void setSIN(int SIN) {
+        this.SIN = SIN;
+    }
+    
+    protected void setCitizenshipStatus(String citizenshipStatus) {
+        this.citizenshipStatus = citizenshipStatus;
+    }
+    
     @Override
     public void login() {
         throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
@@ -54,3 +84,4 @@ public class Employee extends User implements Staff {
     }
     
 }
+
