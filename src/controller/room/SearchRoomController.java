5abe18fdcd378a3b8de69808bae20a77e42b4f4f
Felacc <98856469+Felacc@users.noreply.github.com>
2025-03-23 22:46:22 -0700

    Implement SearchRoomController

--- src/controller/room/SearchRoomController.java
+++ src/controller/room/SearchRoomController.java
@@ -1,9 +1,77 @@
 package controller.room;
 
+import dao.RoomDAO;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.List;
+import javax.swing.table.DefaultTableModel;
+import model.Room;
+import view.HotelRoomSearchView;
+
 /**
  *
  * @author felix
  */
 public class SearchRoomController {
-    
+
+    private HotelRoomSearchView hotelRoomSearchView;
+    private RoomDAO roomDAO;
+
+    public SearchRoomController(HotelRoomSearchView hotelRoomSearchView, RoomDAO roomDAO) {
+        this.hotelRoomSearchView = hotelRoomSearchView;
+        this.roomDAO = roomDAO;
+
+        this.hotelRoomSearchView.searchRoomByIDBtnListener(new SearchRoomByIDActionListener());
+        this.hotelRoomSearchView.searchAllRoomsBtnListener(new SearchAllRoomsActionListener());
+    }
+
+    private class SearchAllRoomsActionListener implements ActionListener {
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            DefaultTableModel model = (DefaultTableModel) hotelRoomSearchView.getTblRooms().getModel();
+            model.setRowCount(0);
+
+            List<Room> roomList = roomDAO.fetchAllRooms();
+            for (Room room : roomList) {
+                Object[] row = {
+                    room.getRoomID(),
+                    room.getRoomNumber(),
+                    room.getRoomType(),
+                    room.getPricePerNight(),
+                    room.getStatus(),
+                    room.getUpdatedBy()
+                };
+                model.addRow(row);
+
+            }
+        }
+
+    }
+
+    private class SearchRoomByIDActionListener implements ActionListener {
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            int roomID = Integer.valueOf(hotelRoomSearchView.getTxtRoomID().getText());
+
+            Room room = null;
+
+            DefaultTableModel model = (DefaultTableModel) hotelRoomSearchView.getTblRooms().getModel();
+            model.setRowCount(0);
+
+            if (roomID != 0) {
+                room = roomDAO.fetchRoomByRoomID(roomID);
+                Object[] row = {
+                    room.getRoomID(),
+                    room.getRoomNumber(),
+                    room.getRoomType(),
+                    room.getPricePerNight(),
+                    room.getStatus(),
+                    room.getUpdatedBy()
+                };
+                model.addRow(row);
+            }
+        }
+    }
 }
